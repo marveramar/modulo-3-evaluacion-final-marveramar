f5c220585c23fc616bd082c241b642bbb8e3461e
marvermar <m.veramartin@gmail.com>
2020-01-14 16:14:59 +0100

    create docs

---
+++ docs/precache-manifest.4cb88d7a770ca2d057182c9a37f80b70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b70dbdf0932a8d1ba3c2fc21b71ef09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4460f02445e7a2202eb6",
+    "url": "./static/css/main.9eca29e0.chunk.css"
+  },
+  {
+    "revision": "4a882cb819cabc8b4ee2",
+    "url": "./static/js/2.f7c1b52d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.f7c1b52d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4460f02445e7a2202eb6",
+    "url": "./static/js/main.3e8ddf3e.chunk.js"
+  },
+  {
+    "revision": "b36f8198265ef85d440b",
+    "url": "./static/js/runtime-main.8592b347.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logoHeader.75509f2e.png"
+  },
+  {
+    "revision": "e0289c46b45b7ad8ad2d2fb26a1a4f9e",
+    "url": "./static/media/rick-morty-season-4-release-date.e0289c46.png"
+  }
+]);
