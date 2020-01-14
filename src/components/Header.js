17cf7d99bc3ba154970b94cf34c6c5dcff278aa0
marvermar <m.veramartin@gmail.com>
2020-01-14 12:01:58 +0100

    add proptypes to verify props. Add an animation in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logoHeader from '../stylesheet/images/logoHeader.png';
+import icon from '../stylesheet/images/rick-morty-season-4-release-date.png';
 
-const Header = () => <div className="header"> <img className="header__img" src={logoHeader} alt="logo web" /></div>;
+const Header = () => <div className="header"> <img className="head" src={icon} alt="icon head morty" /> <img className="header__img" src={logoHeader} alt="logo web" /></div>;
 
 export default Header;
